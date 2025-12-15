9c156970b7f636ba0678a13bdf66729f702e320b
MScheltjens <mathias.scheltjens@gmail.com>
2025-12-15 15:35:45 +0100

    test: pre-commit flow

--- apps/web/components/cta.tsx
+++ apps/web/components/cta.tsx
@@ -1,10 +1,15 @@
-import { Button } from "@repo/ui/components/button";
-import { PageSectionWrapper } from "./page-section-wrapper";
+import { Button } from '@repo/ui/components/button';
+import { PageSectionWrapper } from './page-section-wrapper';
 
-export const Cta = () =>
-    <PageSectionWrapper title="Bereit loszulegen?" description="Erstellen Sie noch heute Ihr Konto und entdecken Sie alle Funktionen, die wir zu bieten haben." muted>
-        <Button size="lg" variant="secondary">
-            Jetzt starten
-        </Button>
-    </PageSectionWrapper >
-
+export const Cta = () => (
+  <PageSectionWrapper
+    title="Bereit loszulegen?"
+    description="Erstellen Sie noch heute Ihr Konto und entdecken Sie alle Funktionen, die wir zu bieten haben."
+    muted
+  >
+    {/* tiny non-functional change to test pre-commit hooks */}
+    <Button size="lg" variant="secondary">
+      Jetzt starten
+    </Button>
+  </PageSectionWrapper>
+);
