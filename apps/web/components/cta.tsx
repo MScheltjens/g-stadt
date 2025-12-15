a76e96c162ffd77f3ac30314c7c968217274338a
MScheltjens <mathias.scheltjens@gmail.com>
2025-12-15 15:32:31 +0100

    update

--- apps/web/components/cta.tsx
+++ apps/web/components/cta.tsx
@@ -1,14 +1,10 @@
-import { Button } from '@repo/ui/components/button';
-import { PageSectionWrapper } from './page-section-wrapper';
+import { Button } from "@repo/ui/components/button";
+import { PageSectionWrapper } from "./page-section-wrapper";
 
-export const Cta = () => (
-  <PageSectionWrapper
-    title="Bereit loszulegen?"
-    description="Erstellen Sie noch heute Ihr Konto und entdecken Sie alle Funktionen, die wir zu bieten haben."
-    muted
-  >
-    <Button size="lg" variant="secondary">
-      Jetzt starten
-    </Button>
-  </PageSectionWrapper>
-);
+export const Cta = () =>
+    <PageSectionWrapper title="Bereit loszulegen?" description="Erstellen Sie noch heute Ihr Konto und entdecken Sie alle Funktionen, die wir zu bieten haben." muted>
+        <Button size="lg" variant="secondary">
+            Jetzt starten
+        </Button>
+    </PageSectionWrapper >
+
