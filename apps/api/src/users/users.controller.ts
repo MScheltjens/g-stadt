ef2f0f307ea703c89279354e3c8dec48a7033c5c
MScheltjens <mathias.scheltjens@gmail.com>
2025-12-16 13:51:55 +0100

    import removed

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -2,8 +2,6 @@ import type { User, CreateUserDto } from '@repo/types/user';
 
 import { Body, Controller, Post } from '@nestjs/common';
 
-import { db } from '@repo/database';
-
 @Controller('users')
 export class UsersController {
   @Post()
