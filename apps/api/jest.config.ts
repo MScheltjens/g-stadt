c25f06e1926633c8d220e71ba9e0a96121c73dba
MScheltjens <mathias.scheltjens@gmail.com>
2025-12-17 13:16:22 +0100

    config change

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,3 +1,9 @@
 import { nestConfig } from '@repo/jest-config';
 
-export default nestConfig;
+export default {
+  ...nestConfig,
+  moduleNameMapper: {
+    '^@repo/database$': '<rootDir>/../../../packages/database/dist/index.js',
+    '^@repo/types$': '<rootDir>/../../../packages/types/dist/index.js',
+  },
+};
