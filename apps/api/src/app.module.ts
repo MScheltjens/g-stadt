6d4aeff8a019ecd6bcc2c4f5d46917c28c164395
MScheltjens <mathias.scheltjens@gmail.com>
2025-12-17 11:45:35 +0100

    fix:imports api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 
-import { AppService } from './app.service.js';
-import { AppController } from './app.controller.js';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
