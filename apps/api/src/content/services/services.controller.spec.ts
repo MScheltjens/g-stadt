bd458258818a1b9b48490a7f219dea54b30c3e7c
MScheltjens <mathias.scheltjens@gmail.com>
2026-01-13 16:43:32 +0100

    err fix

--- apps/api/src/content/services/services.controller.spec.ts
+++ apps/api/src/content/services/services.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
+import { PrismaModule } from '@/content/prisma/prisma.module';
 
 describe('ServicesController', () => {
   let controller: ServicesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [ServicesController],
       providers: [ServicesService],
     }).compile();
--- apps/api/src/content/services/services.service.spec.ts
+++ apps/api/src/content/services/services.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServicesService } from './services.service';
+import { PrismaModule } from '@/content/prisma/prisma.module';
 
 describe('ServicesService', () => {
   let service: ServicesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [ServicesService],
     }).compile();
 
