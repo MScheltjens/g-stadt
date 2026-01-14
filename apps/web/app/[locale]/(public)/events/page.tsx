d777361191ca5f931fc849e79e9be7f146cdac36
MScheltjens <mathias.scheltjens@gmail.com>
2026-01-14 15:24:03 +0100

    unused vars out

--- apps/web/app/[locale]/(public)/events/page.tsx
+++ apps/web/app/[locale]/(public)/events/page.tsx
@@ -1,11 +1,10 @@
-import { getTranslations, setRequestLocale } from '@repo/i18n/server';
+import { setRequestLocale } from '@repo/i18n/server';
 import type { PageProps } from '@/types/next-page';
 import { ComingSoon } from '@/components/common/coming-soon';
 
 export default async function EventsPage({ params }: PageProps) {
   const { locale } = await params;
   setRequestLocale(locale);
-  const t = await getTranslations('events');
 
   return <ComingSoon />;
 }
