acc836c329af0a50e9977257e7359b78aad332bf
MScheltjens <mathias.scheltjens@gmail.com>
2026-01-18 21:53:58 +0100

    debug added

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -4,10 +4,6 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-    seed: 'ts-node prisma/seed.ts',
-  },
   datasource: {
     // use process, so railway env vars are picked up
     url: process.env.DATABASE_URL,
