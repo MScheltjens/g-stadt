22bd794709b4815aa61f09ac57339e203e309c64
MScheltjens <mathias.scheltjens@gmail.com>
2026-02-16 12:03:51 +0100

    recirect from servicecategorypage

--- apps/web/app/[locale]/(public)/services/[categorySlug]/page.tsx
+++ apps/web/app/[locale]/(public)/services/[categorySlug]/page.tsx
@@ -1,43 +1,23 @@
-import { Metadata } from 'next';
-
-import { PublicPageLayout } from '@/components/layout/public-page-layout';
-import { getCategoryServices } from '@/server/services/services.service';
+import { ROUTES } from '@kwh/constants';
+import { redirect } from '@kwh/i18n';
 
 type CategoryServicesPageProps = {
   params: Promise<{
+    locale: string;
     categorySlug: string;
   }>;
 };
 
-export async function generateMetadata({
-  params,
-}: CategoryServicesPageProps): Promise<Metadata> {
-  const { categorySlug } = await params;
-  const data = await getCategoryServices(categorySlug);
-
-  return {
-    title: data ? `Services in ${data.label} category` : 'Services',
-    description: data
-      ? `Explore our services in the ${data.label} category. Find the right service for your needs and get the help you deserve.`
-      : 'Explore our services. Find the right service for your needs and get the help you deserve.',
-  };
-}
-
 export default async function ServicesCategoryPage({
   params,
 }: CategoryServicesPageProps) {
-  const { categorySlug } = await params;
-  console.log('CategorySlug:', categorySlug);
-  const data = await getCategoryServices(categorySlug);
-  const slugToLabel = { [categorySlug]: data.label };
+  const { locale, categorySlug } = await params;
 
-  return (
-    <PublicPageLayout
-      title={data.label}
-      description={data.description}
-      slugToLabel={slugToLabel}
-    >
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </PublicPageLayout>
-  );
+  redirect({
+    href: {
+      pathname: ROUTES.SERVICES,
+      query: { categories: categorySlug },
+    },
+    locale,
+  });
 }
